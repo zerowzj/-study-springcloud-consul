592b79a6bc56bff3d7b2bc0df5b880ddf5d3494d
wangzhenjun3 <wangzhenjun3@xdf.cn>
2021-01-07 13:51:41 +0800

    更新代码

--- src/main/java/study/springcloud/consul/support/SpringBootCfg.java
+++ src/main/java/study/springcloud/consul/support/SpringBootCfg.java
@@ -1,7 +1,9 @@
 package study.springcloud.consul.support;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication(scanBasePackages = "study.springcloud.consul")
 public class SpringBootCfg {
 
